68d46588d584b2f4248a43223e624ee661677a62
nc6d <6od92002@gmail.com>
2020-10-28 12:35:16 +0200

    1st commit

---
+++ OOP/Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+            long c;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a: ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Enter correct value!");
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter b: ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Enter correct value!");
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter c: ");
+                    c = Convert.ToInt64(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Enter correct value!");
+                }
+            }
+            Console.WriteLine($" Number A: {a}\n Number B: {b}\n Number C: {c}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Task_1
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Привіт, ДонНУ!");
+        }
+    }
+}
