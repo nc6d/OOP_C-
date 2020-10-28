838755b93ec661dc78f7941bf9303a3c130ba5c4
nc6d <6od92002@gmail.com>
2020-10-28 12:43:12 +0200

    1st commit

---
+++ OOP/Task4/Task4.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task4
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            GetData(out double radius, out double x, out double y);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nCircle 1:");
+            Circle circle = new Circle(radius, new Point(x, y));
+            Console.WriteLine(circle);
+            Console.WriteLine($"Square: {circle.Square}\nLength: {circle.Length}\n");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            ZoomFactorGet(out double zoom);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"\nCircle 2:");
+            Circle circle1 = new Circle(circle.Radius * zoom, new Point(x, y));
+            Console.WriteLine(circle1);
+            Console.WriteLine($"Square: {circle1.Square}\nLength: {circle1.Length}\n");
+
+            Console.ReadKey();
+        }
+
+
+        static void GetData(out double radius, out double x, out double y)
+        {
+            const string UNDERLINE = "\x1B[4m";
+            const string RESET = "\x1B[0m";
+            Console.Write($"Press {UNDERLINE}F{RESET} to use file data input or press {UNDERLINE}ANY KEY{RESET} to input from keyboard: ");
+            if (Console.ReadKey().Key == ConsoleKey.F)
+            {
+
+                string path = @"TextFile1.txt";
+                using StreamReader sr = new StreamReader(path, Encoding.Default);
+
+                string temp0 = sr.ReadLine();
+                while (!double.TryParse(temp0, out radius) || radius <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\nRadius value is not correct, check and rewrite your txt file!");
+                    System.Environment.Exit(0);
+                }
+                string temp1 = sr.ReadLine();
+                while (!double.TryParse(temp1, out x))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\nAxis X value is not correct, check and rewrite your txt file!");
+                    Environment.Exit(0);
+                }
+                string temp2 = sr.ReadLine();
+                while (!double.TryParse(temp2, out y))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\nAxis Y value is not correct, check and rewrite your txt file!");
+                    Environment.Exit(0);
+                }
+
+            }
+            else
+            {
+                Console.Write("\nEnter radius: ");
+                var temp0 = Console.ReadLine();
+                while (!double.TryParse(temp0, out radius) || radius <= 0)
+                {
+                    Console.WriteLine("Radius value is not correct, try again");
+                    temp0 = Console.ReadLine();
+                }
+                Console.Write("Enter axis X of center: ");
+                var temp1 = Console.ReadLine();
+                while (!double.TryParse(temp1, out x))
+                {
+                    Console.WriteLine("Axis X value is not correct, try again");
+                }
+                Console.Write("Enter axis Y of center: ");
+                var temp2 = Console.ReadLine();
+                while (!double.TryParse(temp2, out y))
+                {
+                    Console.WriteLine("Axis Y value is not correct, try again");
+                    temp2 = Console.ReadLine();
+                }
+            }
+        }
+
+        static void ZoomFactorGet(out double zoom)
+        {
+            Console.Write("Enter zoom-factor: ");
+            var temp = Console.ReadLine();
+            while (!double.TryParse(temp, out zoom) || zoom <= 0)
+            {
+                Console.WriteLine("Invalid value, try again");
+                temp = Console.ReadLine();
+            }
+        }
+    }
+}
+
+public class Circle
+{
+    public double Radius { get; set; }
+    public Point Center { get; set; }
+    public Circle(double radius, Point center)
+    {
+        Radius = radius;
+        Center = center;
+    }
+
+    public double Square
+    {
+        get { return Math.PI * Math.Pow(Radius, 2); }
+    }
+
+    public double Length
+    {
+        get { return Math.PI * 2 * Radius; }
+    }
+    public override string ToString()
+    {
+        return string.Format($"Radius: {Radius}\nCenter coordinates: {Center}");
+    }
+}
+
+public class Point
+{
+    public double X { get; private set; }
+    public double Y { get; private set; }
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString()
+    {
+        return string.Format($"({X}, {Y})");
+    }
+}
---
+++ OOP/Task5/Task5.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Paths to text files
+            string path_x = @"x.txt";
+            string path_y = @"y.txt";
+
+            // Creating empty arrays    
+            double[] arr_x = FilesToGetArrays(path_x);
+            double[] arr_y = FilesToGetArrays(path_y);
+            double[] arr_z = new double[arr_x.Length];
+
+            Console.WriteLine($"Array X: {string.Join(' ', arr_x)}");
+            Console.WriteLine($"Array Y: {string.Join(' ', arr_y)}");
+
+            SubstractionOfArrayX(ref arr_x);
+            SettingOfArrayZ(arr_x, arr_y, ref arr_z);
+
+            Console.WriteLine($"Array X (after changes): {string.Join(' ', arr_x)}");
+            Console.WriteLine($"Array Z: {string.Join(' ', arr_z)}");
+        }
+        static double[] FilesToGetArrays(string link)
+        {
+            string[] fileTxt = File.ReadAllText(link).Split(", ");
+            double[] tempArr = new double[fileTxt.Length];
+            for (int i = 0; i < fileTxt.Length; i++)
+            {
+                try
+                {
+                    tempArr[i] = Convert.ToDouble(fileTxt[i]);
+
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Environment.Exit(0);
+                }
+                catch (InvalidCastException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Environment.Exit(0);
+                }
+            }
+            return tempArr;
+        }
+
+        static void SubstractionOfArrayX(ref double[] arr_x)
+        {
+            for (int i = 0; i < arr_x.Length; i++)
+            {
+                if (arr_x[i] % 5 == 0 && arr_x[i] != 0)
+                {
+                    arr_x[i] = arr_x[i] - 8;
+                }
+            }
+        }
+
+        static void SettingOfArrayZ(double[] arr_x, double[] arr_y, ref double[] arr_z)
+        {
+            for (int i = 0; i < arr_x.Length; i++)
+            {
+                arr_z[i] = (arr_x[i] * arr_x[i] + arr_y[i] * arr_y[i]) / 2;
+            }
+        }
+
+    }
+}
